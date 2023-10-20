b17f05cb2151268536ec6cbc1760e83bf6a03685
Jeff Hong <jeffhong@amazon.com>
2023-10-20 16:10:59 -0500

    add custom repo grouping

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -182,6 +182,7 @@ import { RepoRulesBypassConfirmation } from './repository-rules/repo-rules-bypas
 import { IconPreviewDialog } from './octicons/icon-preview-dialog'
 import { accessibilityBannerDismissed } from './banners/accessibilty-settings-banner'
 import { enableDiffCheckMarksAndLinkUnderlines } from '../lib/feature-flag'
+import { ChangeRepositoryGroup } from './change-repository-group/change-repository-group-dialog'
 
 const MinuteInMilliseconds = 1000 * 60
 const HourInMilliseconds = MinuteInMilliseconds * 60
@@ -2235,6 +2236,15 @@ export class App extends React.Component<IAppProps, IAppState> {
           />
         )
       }
+      case PopupType.ChangeRepositoryGroup: {
+        return (
+          <ChangeRepositoryGroup
+            dispatcher={this.props.dispatcher}
+            repository={popup.repository}
+            onDismissed={onPopupDismissedFn}
+          />
+        )
+      }
       case PopupType.ThankYou:
         return (
           <ThankYou
@@ -3024,6 +3034,17 @@ export class App extends React.Component<IAppProps, IAppState> {
       this.props.dispatcher.changeRepositoryAlias(repository, null)
     }
 
+    const onChangeRepositoryGroup = (repository: Repository) => {
+      this.props.dispatcher.showPopup({
+        type: PopupType.ChangeRepositoryGroup,
+        repository,
+      })
+    }
+
+    const onRemoveRepositoryGroup = (repository: Repository) => {
+      this.props.dispatcher.changeRepositoryGroup(repository, null)
+    }
+
     const items = generateRepositoryListContextMenu({
       onRemoveRepository: this.removeRepository,
       onShowRepository: this.showRepository,
@@ -3034,6 +3055,8 @@ export class App extends React.Component<IAppProps, IAppState> {
       externalEditorLabel: externalEditorLabel,
       onChangeRepositoryAlias: onChangeRepositoryAlias,
       onRemoveRepositoryAlias: onRemoveRepositoryAlias,
+      onChangeRepositoryGroup: onChangeRepositoryGroup,
+      onRemoveRepositoryGroup: onRemoveRepositoryGroup,
       onViewOnGitHub: this.viewOnGitHub,
       repository: repository,
       shellLabel: this.state.selectedShell,
